d7844789353ec6a6107f817eec3193ad5a8bf87a
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-05 17:21:34 -0300

    Criação da classe de Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+    String nome;
+    String cpf;
+
+    public static void main(String[] args) {
+
+    }
+
+    void declaraNome(){
+        nome = "João";
+    }
+
+    void declaraCPF(){
+        cpf = "123.456.789-00";
+    }
+}
