13359a328482295d6abc0d7f8b2ba0c907e2df3c
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-10 16:08:56 -0300

    Criação do print do nome do cliente

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Cliente meuCliente = new Cliente("João", "123.456.789-00");
         Conta minhaConta = new Conta(1, 500.00, meuCliente);
+        System.out.println(minhaConta.getCliente().getNome());
     }
 }
