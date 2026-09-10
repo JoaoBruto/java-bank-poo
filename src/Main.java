6f41f764918e2c816ad8c899f537206d4d617514
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-10 16:03:30 -0300

    Criação do objeto Conta e inserção de valroes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Cliente meuCliente = new Cliente("João", "123.456.789-00");
-
+        Conta minhaConta = new Conta(1, 500.00, meuCliente);
     }
 }
