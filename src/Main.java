db5bbbda399670a43f698e368e8a7dfc761ec9ed
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-10 16:00:25 -0300

    Passando os valores de Cliente

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-
-    }
-    public Cliente(String nome, String cpf) {
-
+        Cliente MeuCliente = new Cliente("João", "123.456.789-00");
+        
     }
 }
