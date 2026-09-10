741e46096bdaefc96692829b46b67c8121e3e174
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-10 15:39:58 -0300

    Criação dos construtores de 'id', 'saldo', 'cliente'

--- src/Conta.java
+++ src/Conta.java
@@ -2,4 +2,11 @@ public class Conta {
     private int id;
     private double saldo;
     private Cliente cliente;
+
+    public Conta(Integer id, double saldo, Cliente cliente) {
+        this.id = id;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
 }
