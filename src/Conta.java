1982d983e3c854c5897636603c652a46a3694f6a
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-11 15:39:40 -0300

    Import da função java Scanner e teste

--- src/Conta.java
+++ src/Conta.java
@@ -1,3 +1,4 @@
+
 public class Conta {
     private int id;
     private double saldo;
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Cliente meuCliente = new Cliente("João", "123.456.789-00");
         Conta minhaConta = new Conta(1, 500.00, meuCliente);
         System.out.println(minhaConta.getCliente().getNome());
         System.out.println(minhaConta.getCliente().getCpf());
-        System.out.println(minhaConta.sacar(100));
+        System.out.println(minhaConta.depositar(100));
+        double valorDepositado = scanner.nextDouble();
+        System.out.print(valorDepositado);
+
 
     }
 }
