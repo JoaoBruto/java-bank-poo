f66c07226120102c1e068786831e41d12409fdd5
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-11 15:48:54 -0300

    Sistema de escolha de opções pro usuário

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,12 @@ public class Main {
         double valorDepositado = scanner.nextDouble();
         System.out.print(valorDepositado);
 
+        while (true) {
+            System.out.println("Escolha entre as opções: 1- Sacar \n 2- Depositar \n 3- Consultar Saldo \n 4- Sair");
+            System.out.print("Digite o número da sua opção: \n");
+            
+        }
+
 
     }
 }
