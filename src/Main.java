dc887de5d5e34e82b956157d139ea8314ea1b845
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-11 15:58:17 -0300

    Leitura das escolhas do usuário usando 'switch'

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,28 @@ public class Main {
         Conta minhaConta = new Conta(1, 500.00, meuCliente);
         System.out.println(minhaConta.getCliente().getNome());
         System.out.println(minhaConta.getCliente().getCpf());
-        System.out.print("Digite o valor do depósito");
+        System.out.print("Digite o valor do depósito: \n");
         double valorDepositado = scanner.nextDouble();
         System.out.print(valorDepositado);
 
         while (true) {
-            System.out.println("Escolha entre as opções: 1- Sacar \n 2- Depositar \n 3- Consultar Saldo \n 4- Sair");
+            System.out.println("Escolha entre as opções: \n 1- Sacar \n 2- Depositar \n 3- Consultar Saldo \n 4- Sair");
             System.out.print("Digite o número da sua opção: \n");
-            int opçãoEscolhida = scanner.nextInt();
-            
+            int opcaoEscolhida = scanner.nextInt();
+            switch (opcaoEscolhida) {
+                case 1:
+                    System.out.println("Você escolheu opção 1");
+                    break;
+                case 2:
+                    System.out.println("Você escolheu opção 2");
+                    break;
+                case 3:
+                    System.out.println("Você escolheu opção 3");
+                    break;
+                case 4:
+                    System.out.println("Você escolheu opção 4");
+                    break;
+            }
         }
 
 
