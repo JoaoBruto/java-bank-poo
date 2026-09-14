d8c4ae690d9a86b62f84b0fb828eb842bdb00885
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-14 16:10:42 -0300

    Criação do sistema de juros aplicados ao saldo da Conta Poupança

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -2,7 +2,12 @@ public class ContaPoupanca extends Conta{
     private double taxaJuros;
     public ContaPoupanca(int id, double saldo, Cliente cliente, double taxaJuros){
         super(id, saldo, cliente);
+        this.taxaJuros = taxaJuros;
+    }
 
+    public double renderJuros(){
+        saldo = saldo + (saldo * taxaJuros);
+        return saldo;
     }
 
 }
